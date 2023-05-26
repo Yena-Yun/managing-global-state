0a111a026375655160d231b34955a8250f814418
Yena-Yun <cheryleduart@gmail.com>
2023-05-26 16:57:16 +0900

    Comment: _app.js에 items 객체에 담기는 형태를 주석으로 명시

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,20 @@ import { useState } from 'react';
 import CartContext from '../components/context/cartContext';
 import Head from 'next/head';
 import Navbar from '../components/Navbar';
+import itemsArray from '../data/items';
 
 function MyApp({ Component, pageProps }) {
   const [items, setItems] = useState({});
-  
+
+  /**
+   * 장바구니 수량 추가 시 items 객체에 담기는 형태 ({ id: 수량 })
+   * 
+   * {
+   *  "8321-k532": 4,
+   *  "9126-b921": 2
+   * }
+   */
+
   return (
     <>
       <Head>
