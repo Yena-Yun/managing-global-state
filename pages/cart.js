46ca38f25e3a095ba22cf80f025a931738d6e551
Yena-Yun <cheryleduart@gmail.com>
2023-05-26 16:58:51 +0900

    Feat: 결제 화면(cart)에서 장바구니 목록과 수량, 총 금액 표시

--- pages/cart.js
+++ pages/cart.js
@@ -1,14 +1,30 @@
-// eslint-disable-next-line no-unused-vars
+import { useContext } from 'react';
+import CartContext from '../components/context/cartContext';
 import data from '../data/items';
 
+/* 매번 선언될 필요가 없는 함수이므로 컴포넌트 바깥에 선언 */
+function getFullItem(id) {
+  const itemIndex = data.findIndex((item) => item.id === id);
+  return data[itemIndex];
+}
+
 function Cart() {
+  const { items } = useContext(CartContext);
+
+  const totalPrice = Object.keys(items)
+    .map((id) => getFullItem(id).price * items[id])
+    .reduce((acc, cur) => acc + cur, 0);
+  
+  const itemInfo = Object.keys(items).map((id) => {
+    const item = getFullItem(id); // 상품 정보
+    return { item, amount: items[id] }; // + 상품 수량
+  })
+
   return (
     <div>
-      <h1 className="text-xl font-bold"> Total: ${/* To be implemented */} </h1>
+      <h1 className='text-xl font-bold'> Total: ${totalPrice} </h1>
       <div>
-        {[
-          /* To be implemented */
-        ].map(({ item, amount }) => (
+        {itemInfo.map(({ item, amount }) => (
           <div key={item.id}>
             x{amount} {item.name} (${amount * item.price})
           </div>
